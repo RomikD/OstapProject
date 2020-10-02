ed01beb6dc56c167f285be0ad694ba45212165d2
Roman Dudnyk <dudnyk.rb@gmail.com>
2020-10-03 00:09:05 +0300

    added mobile version of the main page and 4 pages which include 2 page-templates.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './scss/index.scss'
 console.log('test')
+
+$('.toggle').on('click', function () {
+    console.log('menu_active');
+    $(".main-menu").toggleClass('menu_active');
+
+    $(".menu-icon").toggleClass('menu-icon-active');
+
+});
