f3f595de500618d2aeccc2bce543c3661d78d1bc
Roman Dudnyk <dudnyk.rb@gmail.com>
2020-10-12 00:16:31 +0300

    added 2 pages with tabs and 2 pop-up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import './scss/index.scss'
-console.log('test')
-
-$('.toggle').on('click', function () {
-    console.log('menu_active');
-    $(".main-menu").toggleClass('menu_active');
-
-    $(".menu-icon").toggleClass('menu-icon-active');
-
-});
+import 'bootstrap';
